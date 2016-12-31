a9923c693b46a3026c7e43dcb73e567926997011
SciLor <scilor@users.noreply.github.com>
2016-12-31 13:22:32 +0100

    Initial Commit

---
+++ FileFormats/PIZ/FileHeader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SciLors_Mashed_File_Extractor.FileFormats.PIZ {
+    public class FileHeader {
+        public String fileName { get; set; }
+        public int fileOffset { get; set; }
+        public int fileSize { get; set; }
+        public int fileUnknown { get; set; }
+
+        public FileHeader(byte[] header) {
+            if (header.Length != MagicNumbers.OFFSET_FILE_HEADER_SIZE)
+                throw new IndexOutOfRangeException("File header must be " + MagicNumbers.OFFSET_FILE_HEADER_SIZE + "bytes long");
+            fileName = extractFileName(header);
+            fileOffset = extractFileOffset(header);
+            fileSize = extractFileSize(header);
+            fileUnknown = extractFileUnknown(header);
+        }
+        private String extractFileName(byte[] header) {
+            StringBuilder fileNameBuilder = new StringBuilder();
+            for (int i = 0; i < MagicNumbers.OFFSET_FILE_HEADER_FILE_NAME_MAX_SIZE; i++) {
+                if (header[i] != 0x00) {
+                    fileNameBuilder.Append(Convert.ToChar(header[i]));
+                } else {
+                    break;
+                }
+            }
+            return fileNameBuilder.ToString();
+        }
+
+        private int extractFileOffset(byte[] header) {
+            int offset = header[MagicNumbers.OFFSET_FILE_HEADER_FILE_OFFSET + 0] << 0;
+            offset += header[MagicNumbers.OFFSET_FILE_HEADER_FILE_OFFSET + 1] << 8;
+            offset += header[MagicNumbers.OFFSET_FILE_HEADER_FILE_OFFSET + 2] << 16;
+            offset += header[MagicNumbers.OFFSET_FILE_HEADER_FILE_OFFSET + 3] << 24;
+            return offset;
+        }
+
+        private int extractFileSize(byte[] header) {
+            int size = header[MagicNumbers.OFFSET_FILE_HEADER_FILE_SIZE + 0] << 0;
+            size += header[MagicNumbers.OFFSET_FILE_HEADER_FILE_SIZE + 1] << 8;
+            size += header[MagicNumbers.OFFSET_FILE_HEADER_FILE_SIZE + 2] << 16;
+            size += header[MagicNumbers.OFFSET_FILE_HEADER_FILE_OFFSET + 3] << 24;
+            return size;
+        }
+
+        private int extractFileUnknown(byte[] header) {
+            int size = header[MagicNumbers.OFFSET_FILE_HEADER_FILE_UNKNOWN + 0] << 0;
+            size += header[MagicNumbers.OFFSET_FILE_HEADER_FILE_UNKNOWN + 1] << 8;
+            size += header[MagicNumbers.OFFSET_FILE_HEADER_FILE_UNKNOWN + 2] << 16;
+            size += header[MagicNumbers.OFFSET_FILE_HEADER_FILE_UNKNOWN + 3] << 24;
+            return size;
+        }
+
+    }
+}
---
+++ FileFormats/PIZ/PIZFile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SciLors_Mashed_File_Extractor.FileFormats.PIZ {
+    public class PIZFile : MashedFile {
+        public string filePath { get; set; }
+        public string fileName { get; set; }
+        public FileSignature fileSignature { get; set; }
+        public List<FileHeader> files { get; set; }
+        public PIZFile(string path) {
+            filePath = path;
+            fileName = Path.GetFileName(path);
+
+            using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read)) {
+                byte[] signatureBytes = new byte[FileSignature.Length];
+
+                readBytesSafe(stream, signatureBytes);
+                fileSignature = new FileSignature(signatureBytes);
+
+                stream.Position = MagicNumbers.OFFSET_FILE_HEADER; //Jump to File Table
+                byte[] headerBytes = new byte[MagicNumbers.OFFSET_FILE_HEADER_SIZE];
+                files = new List<FileHeader>();
+                while (true) {
+                    readBytesSafe(stream, headerBytes);
+                    if (headerBytes[0] == 0x00)
+                        break;
+                    files.Add(new FileHeader(headerBytes));
+                }
+
+            }
+        }
+        private void readBytesSafe(FileStream stream, byte[] target) {
+            int n = 0;
+            int bytesToRead = target.Length;
+            int bytesRead = 0;
+            while (bytesToRead > 0) {
+                n = stream.Read(target, bytesRead, bytesToRead);
+                if (n == 0)
+                    throw new FileFormatException("File too small");
+                bytesRead += n;
+                bytesToRead -= n;
+            }
+        }
+    }
+}
